af4818ca2d796a45ce00f22d8c5ac785394e010b
arash.r400@gmail.com <arash.r400@gmail.com>
2020-10-12 23:47:48 +0330

    sign up feature first phase added

---
+++ belphi-laravel/resources/js/components/SignupFormComponent.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="col-md-6">
+
+        <h3>Sign up </h3>
+
+        <form>
+            <div class="form-group">
+                <label for="email"> Your email </label>
+                <input v-model="email" class="form-control" type="email" name="email" id="email">
+            </div>
+
+            <div class="form-group">
+                <label for="name"> Your name </label>
+                <input v-model="name" class="form-control" type="text" name="name" id="name">
+            </div>
+
+            <div class="form-group">
+                <label for="id"> Your id </label>
+                <input v-model="id" class="form-control" type="text" name="id" id="id">
+            </div>
+
+            <div class="form-group">
+                <label for="phone"> Your phone </label>
+                <input v-model="phone" class="form-control" type="text" name="phone" id="phone">
+            </div>
+
+            <div class="form-group">
+                <label for="password"> Your password </label>
+                <input v-model="password" class="form-control" type="password" name="password" id="password">
+            </div>
+
+            <div class="form-group">
+                <label for="bio"> Your bio </label>
+                <input v-model="bio" class="form-control" type="plain-text" name="bio" id="bio">
+            </div>
+
+            <button type="submit" class="btn btn-lg" @click="formSignup" style="color:white; background-color:#a21b24;">
+                Sign up
+            </button>
+            <button type="submit" class="btn btn-lg" @click="undo" style="color:white; background-color:#a21b24;">
+                Back
+            </button>
+            <!-- Below ! -->
+            <input type="hidden" name="_token" value="">
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            name: '',
+            id: '',
+            email: '',
+            phone: '',
+            bio: null,
+            password: ''
+
+        }
+    },
+    methods: {
+        formSignup() {
+
+            axios.post('/api/signup', {
+                name: this.name,
+                id: this.id,
+                password: this.password,
+                email: this.email,
+                phone: this.phone,
+                bio: this.bio
+            })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        undo(){
+            //Must use router i think
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- belphi-laravel/resources/js/components/StartingComponent.vue
+++ belphi-laravel/resources/js/components/StartingComponent.vue
@@ -9,76 +9,76 @@
         <div class="div_sign">
             <h1 class="header_sign"><strong>Belphi</strong></h1>
 
-            <div class="inner_div_sign">
+            <div v-if="showButtons" class="inner_div_sign">
                 <b-button type="submit" variant="outline-warning" class="btn_sign font">Sign in</b-button>
-                <b-button type="submit" variant="outline-warning" class="btn_sign font" @click="signup">Sign up</b-button>
+                <b-button type="submit" variant="outline-warning" class="btn_sign font" @click="signup">Sign up
+                </b-button>
             </div>
+            <div v-if="showSignup" class="d-flex justify-content-center">
+                <signup-form-component></signup-form-component>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
 import axios from "axios";
-    export default {
-        methods:{
-          signup(){
-              axios.post('/api/signup', {
-                  id: 'rezaR',
-                  name: 'Reza',
-                  bio: '',
-                  email: 'a@b.com',
-                  phone: '09121212'
-              })
-                  .then(function (response) {
-                      console.log(response);
-                  })
-                  .catch(function (error) {
-                      console.log(error);
-                  });
-          }
-        },
-        mounted() {
-            console.log('Component mounted.')
+
+export default {
+    data() {
+        return {
+            showButtons: true,
+            showSignin: false,
+            showSignup: false
+        }
+    },
+    methods: {
+        signup() {
+            this.showButtons = false;
+            this.showSignup = true;
         }
     }
+
+}
 </script>
 
 <style scoped>
-.div_sign{
-  position: absolute;
-  text-align: center;
-  width: 40%;
-  height: 40%;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.div_sign {
+    position: absolute;
+    text-align: center;
+    width: 40%;
+    height: 40%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 
-.header_sign{
+.header_sign {
     text-shadow: 2px 2px #ffa500;;
 }
 
-.inner_div_sign{
-  width: 100%;
-  height: 80%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.inner_div_sign {
+    width: 100%;
+    height: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.btn_sign{
+.btn_sign {
     text-align: center;
     padding: 8%;
     margin: 4%;
 }
 
-.font{
+.font {
     font-size: 1.2em;
     font-weight: bold;
 }
 
 @media only screen and (max-width: 400px) {
-    .font{
+    .font {
         font-size: 0.5em;
     }
 }
