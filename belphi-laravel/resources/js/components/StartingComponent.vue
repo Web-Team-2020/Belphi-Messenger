36437befe0fee6e073affc6e0f030c56487fb8c6
mahsahsi <mahsahsii@gmail.com>
2020-10-14 20:00:39 +0330

    set image for background of starting page

--- belphi-laravel/resources/js/components/StartingComponent.vue
+++ belphi-laravel/resources/js/components/StartingComponent.vue
@@ -67,8 +67,8 @@ export default {
 .div_sign {
     position: absolute;
     text-align: center;
-    width: 40%;
-    height: 40%;
+    width: 50%;
+    height: 50%;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
